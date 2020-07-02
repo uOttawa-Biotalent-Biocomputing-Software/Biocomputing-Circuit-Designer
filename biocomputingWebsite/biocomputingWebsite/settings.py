baf93cbc3f8f037b2a0a943e6724b0dd63c8bb09
Sebastien Girard <sebastiengrd@gmail.com>
2020-07-02 15:06:04 -0400

    created simulation app

--- biocomputingWebsite/biocomputingWebsite/settings.py
+++ biocomputingWebsite/biocomputingWebsite/settings.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'usersApp.apps.usersAppConfig',
+    'simulation.apps.simulationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
