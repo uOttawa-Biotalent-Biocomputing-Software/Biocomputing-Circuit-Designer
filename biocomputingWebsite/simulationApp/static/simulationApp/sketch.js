5cdf9a91737a6b5aa266b07021fa92877c0dc10a
Sebastien Girard <sebastiengrd@gmail.com>
2020-07-03 15:33:45 -0400

    added drag and drop feature

--- biocomputingWebsite/simulationApp/static/simulationApp/sketch.js
+++ biocomputingWebsite/simulationApp/static/simulationApp/sketch.js
@@ -8,20 +8,29 @@ const s = ( sketch ) => {
   sketch.wanted_width = 0;
   sketch.getDimensions();
 
+  sketch.allComponents = [];
 
   sketch.setup = () => {
     let cnv = sketch.createCanvas(sketch.wanted_width, sketch.wanted_height);
     // cnv.parent("myContainer");
     sketch.pixelDensity(1);
 
-    img = sketch.loadImage(im);
+    
+    sketch.allComponents.push(new Component(im, 0, 0));
+    sketch.allComponents.push(new Component(im, 500, 500));
+    sketch.allComponents.push(new Component(im, 200, 400));
+    sketch.allComponents.push(new Component(im, 600, 500));
     
   }
 
 
   sketch.draw = () => {
     sketch.background(120);
-    sketch.image(img, 0, 0);
+
+    for (let comp of sketch.allComponents) {
+      comp.show();
+    }
+    //sketch.image(img, sketch.mouseX - 150, sketch.mouseY - 50, 300, 100);
 
   }
   
@@ -30,6 +39,31 @@ const s = ( sketch ) => {
     sketch.resizeCanvas(sketch.wanted_width, sketch.wanted_height, true);
   }
 
+  class Component {
+    constructor(path, initialX, initialY) {
+      this.img = sketch.loadImage(path);;
+      this.w = 300;
+      this.h = 100;
+      this.x = initialX;
+      this.y = initialY;
+
+    }
+
+    show() {
+      if (this.isMouseOver()) {
+        this.x = sketch.mouseX - this.w/2;
+        this.y = sketch.mouseY - this.h/2
+      }
+      sketch.image(this.img, this.x, this.y, this.w, this.h);
+    }
+
+    isMouseOver() {
+      if(this.x < sketch.mouseX && sketch.mouseX < this.x+this.w && this.y < sketch.mouseY && sketch.mouseY < this.y+this.h && sketch.mouseIsPressed) {
+        return true;
+      }
+      return false;
+    }
+  }
 
 }
 // create the canvas with the sketch
