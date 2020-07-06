0067d718f6004c5f22ecb2173edf5abfa27a52f3
Sebastien Girard <sebastiengrd@gmail.com>
2020-07-06 10:26:47 -0400

    moved class Component outside p5.js sketch

--- biocomputingWebsite/simulationApp/static/simulationApp/sketch.js
+++ biocomputingWebsite/simulationApp/static/simulationApp/sketch.js
@@ -1,3 +1,51 @@
+class Component {
+  constructor(path, initialX, initialY, id, sketch) {
+    this.img = sketch.loadImage(path);;
+    this.w = 300;
+    this.h = 100;
+    this.x = initialX;
+    this.y = initialY;
+    this.move = false
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.id = id;
+    this.sketch = sketch;
+  }
+
+  // show component on the canvas
+  show() {
+    if (this.move) {
+      this.x = sketch.mouseX - this.w/2 + this.offsetX;
+      this.y = sketch.mouseY - this.h/2 + this.offsetY;
+    }
+    this.sketch.image(this.img, this.x, this.y, this.w, this.h);
+  }
+
+  // return true if mouse is over the component
+  isMouseOver() {
+    if(this.x < this.sketch.mouseX && this.sketch.mouseX < this.x+this.w && this.y < this.sketch.mouseY && this.sketch.mouseY < this.y+this.h && this.sketch.mouseIsPressed) {
+      return true;
+    }
+    return false;
+  }
+
+  // calculate where the mouse is located relative to x and y of the component
+  calculateOffset() {
+    this.offsetX = -this.sketch.mouseX + this.x + this.w/2;
+    this.offsetY = -this.sketch.mouseY + this.y + this.h/2;
+  }
+  
+  // when mouse release, we need to reajust the x and y of the component with the offset
+  resetOffset() {
+    this.x = this.sketch.mouseX - this.w/2 + this.offsetX;
+    this.y = this.sketch.mouseY - this.h/2 + this.offsetY;
+    
+    // set the offset at 0 after ajusting x and y
+    this.offsetX = 0;
+    this.offsetY = 0;
+  }
+}
+
 const s = ( sketch ) => {
   
   
@@ -19,10 +67,10 @@ const s = ( sketch ) => {
     sketch.pixelDensity(1);
 
     
-    sketch.allComponents.push(new Component(im, 0, 0, 0));
-    sketch.allComponents.push(new Component(im, 500, 500, 1));
-    sketch.allComponents.push(new Component(im, 200, 400, 2));
-    sketch.allComponents.push(new Component(im, 600, 500, 3));
+    sketch.allComponents.push(new Component(im, 0, 0, 0, sketch));
+    sketch.allComponents.push(new Component(im, 500, 500, 1, sketch));
+    sketch.allComponents.push(new Component(im, 200, 400, 2, sketch));
+    sketch.allComponents.push(new Component(im, 600, 500, 3, sketch));
     
   }
 
@@ -62,53 +110,6 @@ const s = ( sketch ) => {
         comp.resetOffset();
         comp.move = false;
       }
-    }
-  }
-
-  class Component {
-    constructor(path, initialX, initialY, id) {
-      this.img = sketch.loadImage(path);;
-      this.w = 300;
-      this.h = 100;
-      this.x = initialX;
-      this.y = initialY;
-      this.move = false
-      this.offsetX = 0;
-      this.offsetY = 0;
-      this.id = id;
-    }
-
-    // show component on the canvas
-    show() {
-      if (this.move) {
-        this.x = sketch.mouseX - this.w/2 + this.offsetX;
-        this.y = sketch.mouseY - this.h/2 + this.offsetY;
-      }
-      sketch.image(this.img, this.x, this.y, this.w, this.h);
-    }
-
-    // return true if mouse is over the component
-    isMouseOver() {
-      if(this.x < sketch.mouseX && sketch.mouseX < this.x+this.w && this.y < sketch.mouseY && sketch.mouseY < this.y+this.h && sketch.mouseIsPressed) {
-        return true;
-      }
-      return false;
-    }
-
-    // calculate where the mouse is located relative to x and y of the component
-    calculateOffset() {
-      this.offsetX = -sketch.mouseX + this.x + this.w/2;
-      this.offsetY = -sketch.mouseY + this.y + this.h/2;
-    }
-    
-    // when mouse release, we need to reajust the x and y of the component with the offset
-    resetOffset() {
-      this.x = sketch.mouseX - this.w/2 + this.offsetX;
-      this.y = sketch.mouseY - this.h/2 + this.offsetY;
-      
-      // set the offset at 0 after ajusting x and y
-      this.offsetX = 0;
-      this.offsetY = 0;
     }
   }
 
