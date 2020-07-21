009629f882515717975af22fc57e7418573375c3
Sebastien Girard <sebastiengrd@gmail.com>
2020-07-21 13:04:27 -0400

    added mouse on bar variable and update it

--- biocomputingWebsite/simulationApp/static/simulationApp/topBar.js
+++ biocomputingWebsite/simulationApp/static/simulationApp/topBar.js
@@ -3,10 +3,14 @@ class topBar {
         this.heightPercent = heightPercent;
         this.sketch = sketch;
         this.h = this.heightPercent * this.sketch.wanted_height / 100;
+        this.mouseOnBar = false;
     }
 
     update() {
         this.show();
+        this.mouseOnBar = (this.sketch.mouseY > 0 && this.sketch.mouseY < this.h);
+        
+
     }
 
     show(){
