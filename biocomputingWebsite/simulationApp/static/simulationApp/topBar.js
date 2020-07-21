f0ac671e931d26ba2c9bb0439e6be8a4231f7b79
Sebastien Girard <sebastiengrd@gmail.com>
2020-07-21 14:30:47 -0400

    added top bar icons

--- biocomputingWebsite/simulationApp/static/simulationApp/topBar.js
+++ biocomputingWebsite/simulationApp/static/simulationApp/topBar.js
@@ -4,13 +4,21 @@ class topBar {
         this.sketch = sketch;
         this.h = this.heightPercent * this.sketch.wanted_height / 100;
         this.mouseOnBar = false;
+        this.icons = []
+
+        for (let i = 0; i<3; i++) {
+            this.icons.push(new topBarElement((this.h)*(i), 2, this.h-2, this.sketch));
+        }
     }
 
     update() {
         this.show();
         this.mouseOnBar = (this.sketch.mouseY > 0 && this.sketch.mouseY < this.h);
-        
 
+        for(let icon of this.icons) {
+            icon.update();
+        }
+        
     }
 
     show(){
