991c20cff9f22fe675e580b78e10b33737316f47
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2020-07-22 10:04:56 -0400

    Added Side Panel for Molecular Species to be added to canvas

--- biocomputingWebsite/simulationApp/models.py
+++ biocomputingWebsite/simulationApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    projectName = models.CharField(max_length=200)
+
+    def getName(self):
+        return self.projectName
