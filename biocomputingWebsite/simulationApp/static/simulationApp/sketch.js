91001aac32409dfcd5779c971dfe8387c2a0b92f
Sebastien Girard <sebastiengrd@gmail.com>
2020-08-07 12:13:49 -0400

    remove useless files

--- biocomputingWebsite/simulationApp/static/simulationApp/sketch.js
+++ biocomputingWebsite/simulationApp/static/simulationApp/sketch.js
@@ -16,8 +16,6 @@ const s = ( sketch ) => {
   sketch.allComponents = [];
   sketch.allEdges = [];
 
-  sketch.topBar = new topBar(5, sketch);
-
   // p5.js execute this method once at the loading of the page
   sketch.setup = () => {
     let cnv = sketch.createCanvas(sketch.wanted_width, sketch.wanted_height);
@@ -40,8 +38,6 @@ const s = ( sketch ) => {
       edge.update();
     }
     
-    sketch.topBar.update();
-
   }
 
   // handle resize events
@@ -63,10 +59,6 @@ const s = ( sketch ) => {
       sketch.drag = null;
     }
 
-    if (sketch.topBar.mouseOnBar) {
-      sketch.topBar.mousePressed();
-      return;
-    }
 
     sketch.backgroundPressed = true;
     // console.log('Canvas is Clicked');
@@ -154,7 +146,6 @@ const s = ( sketch ) => {
     // call the resize method in each component
     if(sketch.mouseX<0){return;};
     if(sketch.mouseY<0){return;};
-    if (sketch.topBar.mouseOnBar){return;};
     sketch.grid.resize(-event.delta/1000) 
   }
 
